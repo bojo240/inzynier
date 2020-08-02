29d34ec368207d5c5ed4ff45b9c4698c9575a54d
bojo240 <bojo240@gmail.com>
2020-08-02 20:09:07 +0200

    dziala update i delete

--- app/flask_sql.py
+++ app/flask_sql.py
@@ -25,7 +25,6 @@ def JSONLogin():
     try:
         APIconnection = pyodbc.connect(
             "Driver={SQL Server};Server=DESKTOP-BQPOPVS;PORT=1433;Database=Inzynier;UID=" + login + ";PWD=" + password)
-
         cur = APIconnection.cursor()
         cur.execute("select * from gmina")
         cur.close
@@ -47,18 +46,21 @@ def JSONMainTable(table_name, id, start_date):
     global APIconnection
     cursor = APIconnection.cursor()
     cursor.execute("SELECT * FROM "+ table_name)
-    cursor.execute("SELECT * FROM "+ table_name + " where " + cursor.description[0][0] + " = " + id + " and " + cursor.description[3][0] + " = '" + start_date + "'")
+    cursor.execute("SELECT * FROM "+ table_name + " where " + cursor.description[0][0] + " = " + id + " and " +
+                   cursor.description[3][0] + " = '" + start_date + "'")
     rows = cursor.fetchall()
     items = [dict(zip([key[0] for key in cursor.description], row)) for row in rows]
     return json.dumps(items), 201
 
-@app.route('/api/<table_name>/<id1>/<start_date1>/<id2>/<start_date2>/<start_date3>', methods=['GET'])
-def JSONRelationalTable(table_name, id1, start_date1, id2, start_date2, start_date3):
+@app.route('/api/<table_name>/<id>/<start_date>/<id1>/<start_date1>/<start_date2>', methods=['GET'])
+def JSONRelationalTable(table_name, id, start_date, id1, start_date1, start_date2):
     global APIconnection
     cursor = APIconnection.cursor()
     cursor.execute("SELECT * FROM "+ table_name)
-    cursor.execute("SELECT * FROM "+ table_name + " where " + cursor.description[0][0] + " = " + id1 + " and " + cursor.description[1][0] + " = '" + start_date1 + "'" + " and " +
-                   cursor.description[2][0] + " = " + id2 + " and " + cursor.description[3][0] + " = '" + start_date2 + "'" + " and " + cursor.description[4][0] + " = '" + start_date3 + "'")
+    cursor.execute("SELECT * FROM "+ table_name + " where " + cursor.description[0][0] + " = " + id + " and " +
+                   cursor.description[1][0] + " = '" + start_date + "'" + " and " +
+                   cursor.description[2][0] + " = " + id1 + " and " + cursor.description[3][0] + " = '" + start_date1 +\
+                   "'" + " and " + cursor.description[4][0] + " = '" + start_date2 + "'")
     rows = cursor.fetchall()
     items = [dict(zip([key[0] for key in cursor.description], row)) for row in rows]
     return json.dumps(items), 201
@@ -88,8 +90,103 @@ def JSONInsertTable(table_name):
     except:
         return Response(status = 400)
 
+@app.route('/api/<table_name>/<id>/<start_date>', methods=['PUT'])
+def JSONUpdateMainTable(table_name, id, start_date):
+    global APIconnection
+    data = request.get_json()
+    col_names = list(data.keys())
+    col_vals = list(data.values())
+    cursor = APIconnection.cursor()
+    cursor.execute("SELECT column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS where upper(table_name) = upper('"\
+                   + table_name + "') order by ordinal_position asc")
+    for row in cursor:
+        for x in range(len(col_names)):
+            if col_names[x] == row[0]:
+                if col_vals[x] == '':
+                    col_vals[x] = "null"
+                elif row[1] in ("date", "nvarchar"):
+                    col_vals[x] = "'" + col_vals[x] + "'"
+                break;
+    print(col_names)
+    print(col_vals)
+    s = "update " + table_name + " set " + col_names[0] + " = " + col_vals[0]
+    for x in range(len(col_names) - 1):
+        s = s + ", " + col_names[x+1] + " = " + col_vals[x+1]
+    s = s + " where id = " + str(id) + " and StartDate = '" + str(start_date) + "'"
+    print(s)
+    try:
+        cursor.execute(s)
+        APIconnection.commit()
+        return Response(status = 200)
+    except:
+        return Response(status = 400)
 
-#       GUI
+@app.route('/api/<table_name>/<id>/<start_date>/<id1>/<start_date1>/<start_date2>', methods=['PUT'])
+def JSONUpdateRelationalTable(table_name, id, start_date, id1, start_date1, start_date2):
+    global APIconnection
+    data = request.get_json()
+    col_names = list(data.keys())
+    col_vals = list(data.values())
+    cursor = APIconnection.cursor()
+    cursor.execute("SELECT column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS where upper(table_name) = upper('"\
+                   + table_name + "') order by ordinal_position asc")
+    for row in cursor:
+        for x in range(len(col_names)):
+            if col_names[x] == row[0]:
+                if col_vals[x] == '':
+                    col_vals[x] = "null"
+                elif row[1] in ("date", "nvarchar"):
+                    col_vals[x] = "'" + col_vals[x] + "'"
+                break;
+    print(col_names)
+    print(col_vals)
+    s = "update " + table_name + " set " + col_names[0] + " = " + col_vals[0]
+    for x in range(len(col_names) - 1):
+        s = s + ", " + col_names[x+1] + " = " + col_vals[x+1]
+    s = s + " where " + col_names[0] + " = " + str(id) + " and " + col_names[1] + " = '" + str(start_date) + "'" + \
+        " and " + col_names[2] + " = " + str(id1) + " and " + col_names[3] + " = '" + str(start_date1) + "'" \
+        + " and " + col_names[4] + " = '" + str(start_date2) + "'"
+    print(s)
+    try:
+        cursor.execute(s)
+        APIconnection.commit()
+        return Response(status = 200)
+    except:
+        return Response(status = 400)
+
+@app.route('/api/<table_name>/<id>/<start_date>', methods=['DELETE'])
+def JSONDeleteMainTable(table_name, id, start_date):
+    global APIconnection
+    cursor = APIconnection.cursor()
+    s = "delete from " + table_name + " where ID = " + str(id) + " and StartDate = '" + str(start_date) + "'"
+    print(s)
+    try:
+        cursor.execute(s)
+        APIconnection.commit()
+        return Response(status = 200)
+    except:
+        return Response(status = 400)
+
+@app.route('/api/<table_name>/<id>/<start_date>/<id1>/<start_date1>/<start_date2>', methods=['DELETE'])
+def JSONDeleteRelationalTable(table_name, id, start_date, id1, start_date1, start_date2):
+    global APIconnection
+    data = request.get_json()
+    col_names = list(data.keys())
+    cursor = APIconnection.cursor()
+    cursor.execute("SELECT column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS where upper(table_name) = upper('"\
+                   + table_name + "') order by ordinal_position asc")
+    s = "delete from " + table_name + " where " + col_names[0] + " = " + str(id) + " and " + col_names[1] + " = '" + str(start_date) + "'" + \
+        " and " + col_names[2] + " = " + str(id1) + " and " + col_names[3] + " = '" + str(start_date1) + "'" \
+        + " and " + col_names[4] + " = '" + str(start_date2) + "'"
+    print(s)
+    try:
+        cursor.execute(s)
+        APIconnection.commit()
+        return Response(status = 200)
+    except:
+        return Response(status = 400)
+
+# GUI
 
 @app.context_processor
 def override_url_for():
@@ -239,16 +336,15 @@ def updateTable(table_name):
                 wherevalue = "'" + wherevalue + "'"
             if wherevalue != '' and wherevalue != "''":
                 where_vals[col_names[x]] = wherevalue
-        updateALL(table_name, set_vals, where_vals)
+        updateALL(table_name, set_vals, where_vals, GUIconnection)
     return render_template("update.html", table_name=table_name,
                            navigation=[str(x + 1) for x in range(len(col_names))],
                            col_names=col_names,
                            table=generateTable(table_name))
 
 
-def updateALL(table_name, set_vals, where_vals):
-    global GUIconnection
-    cursor = GUIconnection.cursor()  
+def updateALL(table_name, set_vals, where_vals, connection):
+    cursor = connection.cursor()
     s = "update " + table_name + " set " + list(set_vals.keys())[0] + " = " + set_vals[list(set_vals.keys())[0]]
     for x in range(len(set_vals)-1):
         s = s + ", " + list(set_vals.keys())[x+1] + " = " + set_vals[list(set_vals.keys())[x+1]]
@@ -268,7 +364,7 @@ def updateALL(table_name, set_vals, where_vals):
         s = s + where_vals[list(where_vals.keys())[x+1]]
 
     cursor.execute(s)
-    GUIconnection.commit()	
+    connection.commit()
     return
 
 
@@ -291,16 +387,15 @@ def deleteTable(table_name):
                 wherevalue = "'" + wherevalue + "'"   
             if wherevalue != '' and wherevalue != "''":
                 where_vals[col_names[x]] = wherevalue
-        deleteALL(table_name, where_vals)
+        deleteALL(table_name, where_vals, GUIconnection)
     return render_template("delete.html", table_name=table_name,
                            navigation=[str(x + 1) for x in range(len(col_names))],
                            col_names=col_names,
                            table=generateTable(table_name))
 
 
-def deleteALL(table_name, where_vals):
-    global GUIconnection
-    cursor = GUIconnection.cursor()  
+def deleteALL(table_name, where_vals, connection):
+    cursor = connection.cursor()
     s = "delete from " + table_name + " where " + list(where_vals.keys())[0]
     if where_vals[list(where_vals.keys())[0]].lower() == "null":
         s = s + " is "
@@ -316,7 +411,7 @@ def deleteALL(table_name, where_vals):
         s = s + where_vals[list(where_vals.keys())[x+1]]
 
     cursor.execute(s)
-    GUIconnection.commit()	
+    connection.commit()
     return
 
 
